2eff0413cc97e263739679fd4430150b7fd99f36
juan-rybczinski <jhreplay.lee@gmail.com>
2020-07-31 16:01:22 +0900

    Notification importance

--- android-kotlin-notifications-master/app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ android-kotlin-notifications-master/app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -72,6 +72,7 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
             applicationContext.getString(R.string.snooze),
             snoozePendingIntent
         )
+        .setPriority(NotificationCompat.PRIORITY_HIGH)
 
     notify(NOTIFICATION_ID, builder.build())
 }
